abe0a7717e41016833629d48793c2efadb5582de
FrontExtreme01 <frontend.extreme@gmail.com>
2025-10-22 08:47:37 -0500

    FT: Add new SVG icons for social media and utility fonts, favicons

---
+++ public/assets/img/allImages.ts
@@ -0,0 +1,21 @@
+import Logo from "./logos/logo-snorkel.png"
+import BgSnorkel from "./bg/snorkel_stock.jpg";
+import BgSnorkelCancun from "./bg/snorkel-in-cancun.webp";
+import BgCorales from "./bg/corales.jpeg";
+import BgFishes from "./bg/pececitos.jpg";
+import BgMap from "./bg/footer-bg.png";
+import IntroSnorkel from "./bg/intro-snorkel.webp";
+import Algas from "./bg/algas.png";
+import Turtle from "./sea-turtle-in-national-reef-park-of-puerto-morelos.webp";
+
+export {
+    Logo,
+    BgSnorkel,
+    BgSnorkelCancun,
+    BgCorales,
+    BgFishes,
+    BgMap,
+    IntroSnorkel,
+    Algas,
+    Turtle
+}
---
+++ src/icons/allIcons.ts
@@ -0,0 +1,39 @@
+// Flags
+import IconFlagUSA from "./flags/usa.svg";
+import IconFlagMEX from "./flags/mex.svg";
+// Icons
+import BranX from "./brand-x.svg";
+import Close from "./close.svg";
+import Facebook from "./facebook.svg";
+import Instagram from "./instagram.svg";
+import Linkedin from "./brand-linkedin.svg";
+import LinkShared from "./link.svg";
+import Mail from "./mail.svg";
+import MapPin from "./map-pin.svg";
+import Menu from "./menu.svg";
+import Phone from "./phone-fill.svg";
+import Share from "./share.svg";
+import Tiktok from "./tiktok.svg";
+import Tripadvisor from "./tripadvisor.svg";
+import World from "./world.svg";
+import Youtube from "./youtube.svg";
+
+export {
+    IconFlagUSA,
+    IconFlagMEX,
+    BranX,
+    Close,
+    Facebook,
+    Instagram,
+    Linkedin,
+    LinkShared,
+    Mail,
+    MapPin,
+    Menu,
+    Phone,
+    Share,
+    Tiktok,
+    Tripadvisor,
+    World,
+    Youtube
+}
