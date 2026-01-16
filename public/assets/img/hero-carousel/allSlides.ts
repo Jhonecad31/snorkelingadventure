6902b06f86d636f32fb62f179df51912fb390d14
FrontExtreme01 <frontend.extreme@gmail.com>
2026-01-16 09:28:32 -0500

    FT: Add Spanish images and update getSlides function for multilingual support

--- public/assets/img/hero-carousel/allSlides.ts
+++ public/assets/img/hero-carousel/allSlides.ts
@@ -1,9 +1,14 @@
+import type { ui } from "@/i18n/ui";
 // Desktop
 import RegularSnorkelDesk from "./discover-the-reef-life-snorkel-puerto-morelos.webp";
 import PrivateSnorkelDesk from "./private-snorkel-in-puerto-morelos.webp";
+import RegularSnorkelDeskES from "./es/discover-the-reef-life-snorkel-puerto-morelos-es.webp";
+import PrivateSnorkelDeskES from "./es/private-snorkel-in-puerto-morelos-es.webp";
 // Mobile
 import RegularSnorkelMob from "./mobile/discover-the-reef-life-snorkel-puerto-morelos-mobile.webp";
 import PrivateSnorkelMob from "./mobile/private-snorkel-in-puerto-morelos-mobile.webp";
+import RegularSnorkelMobES from "./es/mobile/discover-the-reef-life-snorkel-puerto-morelos-mobile-es.webp";
+import PrivateSnorkelMobES from "./es/mobile/private-snorkel-in-puerto-morelos-mobile-es.webp";
 
 const images = {
     en: {
@@ -15,10 +20,20 @@ const images = {
             { img: RegularSnorkelMob, alt: "snorkel in puerto morelos", href: "/snorkel-adventuring-details/" },
             { img: PrivateSnorkelMob, alt: "private snorkel in puerto morelos", href: "/private-snorkeling-in-puerto-morelos/" }
         ]
+    },
+    es: {
+        desktop: [
+            { img: RegularSnorkelDeskES, alt: "snorkel en puerto morelos", href: "/snorkel-adventuring-details/" },
+            { img: PrivateSnorkelDeskES, alt: "snorkel privado en puerto morelos", href: "/private-snorkeling-in-puerto-morelos/" }
+        ],
+        mobile: [
+            { img: RegularSnorkelMobES, alt: "snorkel en puerto morelos", href: "/snorkel-adventuring-details/" },
+            { img: PrivateSnorkelMobES, alt: "snorkel privado en puerto morelos", href: "/private-snorkeling-in-puerto-morelos/" }
+        ]
     }
 };
 
-const getSlides = (lang: keyof typeof images) => {
+const getSlides = (lang: keyof typeof ui) => {
     const categoryImages = images[lang] || images["en"];
 
     if (!categoryImages) {
