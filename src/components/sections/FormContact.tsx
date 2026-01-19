a249a577d8d8242a75b325b73d16611cc80bd2ee
FrontExtreme01 <frontend.extreme@gmail.com>
2026-01-19 15:38:08 -0500

    FT: Enhance Contact and Choose Plan sections with improved layout and form integration; update English localization for Shared Snorkeling Adventure

--- src/components/sections/FormContact.tsx
+++ src/components/sections/FormContact.tsx
@@ -3,6 +3,7 @@ import { actions } from "astro:actions";
 import { Form, Input, Textarea, Button, Modal, ModalContent, ModalBody, useDisclosure } from "@heroui/react";
 
 interface FormContactProps {
+    TITLE_FORM: string;
     i18n: {
         CLOSE_MODAL: string,
         LABELS: {
@@ -41,7 +42,7 @@ interface FormContactProps {
     };
 }
 
-export default function FormContact({ i18n }: FormContactProps) {
+export default function FormContact({ i18n, TITLE_FORM }: FormContactProps) {
 
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [isError, setIsError] = useState(false);
@@ -65,7 +66,8 @@ export default function FormContact({ i18n }: FormContactProps) {
     };
 
     return (
-        <>
+        <div>
+            <h2 className="text-xl/normal lg:text-3xl/relaxed font-bold mt-5">{TITLE_FORM}</h2>
             <Form onSubmit={handleSubmit}>
                 <div className="flex flex-wrap py-5 space-y-8">
                     <Input
@@ -192,6 +194,6 @@ export default function FormContact({ i18n }: FormContactProps) {
                     )}
                 </ModalContent>
             </Modal>
-        </>
+        </div>
     )
 }
